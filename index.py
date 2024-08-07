f263633734148a110ddfda58b2bcdaf7d9c7c7e5
linghucong1999 <2865911620@qq.com>
2024-08-07 16:13:52 +0800

    style: 布局的改变

--- index.py
+++ index.py
@@ -12,21 +12,21 @@ class DataCrawlerApp:
         self.root.title("爬虫数据采集器")
 
         # 创建输入框
-        tk.Label(root, text="请输入isp-token：").grid(row=1, column=0)
+        tk.Label(root, text="请输入isp-token：").grid(row=0, column=0)
         self.token_entry = tk.Entry(root)
-        self.token_entry.grid(row=1, column=1)
+        self.token_entry.grid(row=0, column=1)
 
-        tk.Label(root, text="请输入Cookie:").grid(row=2, column=0)
+        tk.Label(root, text="请输入Cookie:").grid(row=1, column=0)
         self.cookie_entry = tk.Entry(root)
-        self.cookie_entry.grid(row=2, column=1)
+        self.cookie_entry.grid(row=1, column=1)
 
-        tk.Label(root, text="请输入载荷:").grid(row=3, column=0)
+        tk.Label(root, text="请输入载荷:").grid(row=2, column=0)
         self.json_text = scrolledtext.ScrolledText(root, height=10, width=50)
-        self.json_text.grid(row=3, column=1, rowspan=3)
+        self.json_text.grid(row=2, column=1, rowspan=3)
 
         # 创建发送按钮
         tk.Button(root, text="发送", command=self.send_data).grid(
-            row=4, column=1)
+            row=5, column=1)
 
     def send_data(self):
         token = self.token_entry.get()
